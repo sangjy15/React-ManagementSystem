dbf6f850b6fcd4ee9e67850991d872190511efd2
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-08-06 13:01:48 +0900

    customerInfo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Customer from './components/Customent';
+
+const customers =[{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '920112',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길순',
+  'birthday': '890112',
+  'gender': '여자',
+  'job': '무직'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이영희',
+  'birthday': '920112',
+  'gender': '남자',
+  'job': '디자이너'
+}]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's make management system!</h2>
+    <div>
+      {
+        customers.map(item => {
+          return (
+            <Customer 
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              birthday={item.birthday}
+              gender={item.gender}
+              job={item.job}
+              />
+          );
+      })
+    }
     </div>
   );
 }
